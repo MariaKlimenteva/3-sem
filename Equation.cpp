6f256e92686d6ab299f4be11cfa8311997aa8ef5
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-07-20 19:12:39 +0300

    Начальная версия квадратки

---
+++ Equation.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf ("Чтобы решить квадратное уравнение, введите коэффициенты");
+    printf ("a = , b = , c = ", a, b, c);
+    scanf (&a, &b, &c);
+
+    if {a = 0}
+    {
+        if (b = 0)
+        {
+            if (c = 0)
+            {
+                printf ("x - любой");
+            }
+            else
+            {
+                printf ("нет решения");
+            }
+        else
+        {
+           x = -c/b;
+           printf ("Один корень, x = ", x);
+        }
+        }
+    else
+    {
+        Подсчет дискриминанта
+    }
+    }
+    return 0;
+}
+
+int Discr (int a, b, c)
+return ;
+
+//функция решения линейного уравнения
+int linear_equation (int b, int c)
+{
+
+    return ();
+}
+
+
+
+
+
+
+
+
+
+
