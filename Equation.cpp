4243338ba581cb0d06c33204f5ea3be9b30276be
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-07-21 12:46:21 +0300

    Добавила функцию сравнения с погрешностью с помощью abs

--- Equation.cpp
+++ Equation.cpp
@@ -3,6 +3,8 @@
 
 double a, b, c, x;
 double linear_equation(double b, double c);
+#define ERROR 10^-4
+int comp_error(double a, double b);
 int main()
 {
     printf ("To solve the quadratic equation, enter the coefficients:");
@@ -17,6 +19,23 @@ int main()
     {
  //       Подсчет дискриминанта
     }
+/*    switch (errors){
+    case -1:
+        printf ("Infinitely many solutions");
+        break;
+    case 0:
+        printf("");
+        break;
+    case 1:
+        printf("");
+        break;
+    case 2:
+        printf("");
+        break;
+
+    default:
+        printf("");*/
+    }
 
     return 0;
 }
@@ -27,27 +46,32 @@ int main()
 
 double linear_equation (double b, double c)
 {
-        if (b = 0)
+        if (comp_error(b, 0))
         {
-            if (c = 0)
+            if (comp_error(c, 0))
             {
                 printf ("Infinitely many solutions");
-                return (-1);
+                return -1;
             }
             else
             {
                 printf ("There are no solutions");
-                return (2);
+                return 2;
             }
         }
         else
         {
            x = -c/b;
            printf ("One solution, x = ", x);
-           return (1);
+           return 1;
         }
 }
 
+int comp_error(double a, double b){
+    if (abs(a - b) <= ERROR) {
+        return 0;
+    }
+}
 
 
 
