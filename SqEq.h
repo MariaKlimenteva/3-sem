00ebd7b8ffd454c041dd4a861cdf1ed36ad5e6a2
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-08-21 15:33:49 +0300

    Добавила ifndef, define, endif, но в SqEq.cpp как будто не включился Input.h

--- SqEq.h
+++ SqEq.h
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "Input.h"
-#include "Comp_eps.h"
+#ifndef INPUT_H
+#define INPUT_H
+
+#ifndef COMP_EPS_H
+#define COMP_EPS_H
 
 int solve_eq (double a, double b, double c, double *x1, double *x2);
 double linear_equation (double b, double c, double *x1);
 
+#endif
+#endif
