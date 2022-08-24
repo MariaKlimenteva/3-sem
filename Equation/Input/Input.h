922c0c9e9436261bdcbf3e6dffd9a61598b5eaeb
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-08-24 20:34:08 +0300

    Запихнула инклюды в стражи защиты

--- Equation/Input/Input.h
+++ Equation/Input/Input.h
@@ -1,16 +1,16 @@
 
+#ifndef INPUT_H
+#define INPUT_H
+
 #include <time.h>
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 
-#ifndef INPUT_H
-#define INPUT_H
-
 const bool Debug = true;
 const int Max_str = 256;
 
-int input ( const double *a, const double *b, const double *c);
+int input (const double *a, const double *b, const double *c);
 void trolling ();
 void is_trolling();
 int start_trolling (const char ch[][Max_str], const int num_lines);
