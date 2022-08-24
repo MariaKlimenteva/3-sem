4c9ed7aace70e522f2ba1b65ef59eaf9bef5d80a
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-08-24 22:53:23 +0300

    -

--- Equation/SqEq/SqEq.h
+++ Equation/SqEq/SqEq.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "Input.h"
+#include "../Input/Input.h"
 #include "Comp_eps.h"
 
 int solve_eq (double a, double b, double c, double *x1, double *x2);
