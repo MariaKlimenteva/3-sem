ceb7f2e69f2afb04b21d210761eaab24306ac9cf
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-08-27 00:49:40 +0300

    Вынесение юнит тестов в отдельный файл

---
+++ Equation/unit_tests/unit_tests.h
@@ -0,0 +1,9 @@
+
+#ifndef UNIT_TESTS_H
+#define UNIT_TESTS_H
+
+void unit_test (double a, double b, double c, int test_roots, double test_x1, double test_x2);
+
+void unit_Test_fromFile (double a, double b, double c, int test_roots, double test_x1, double test_x2);
+
+#endif // UNIT_TESTS_H
