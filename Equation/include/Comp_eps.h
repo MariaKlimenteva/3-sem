6901e276b543575853012070d52b0f5e84abb1ce
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-08-30 21:43:09 +0300

    Добавила документацию

--- Equation/include/Comp_eps.h
+++ Equation/include/Comp_eps.h
@@ -1,11 +1,17 @@
-
-#include <math.h>
-
+//=============================================================================
 #ifndef COMP_EPS_H
 #define COMP_EPS_H
 
+#include <math.h>
+//-----------------------------------------------------------------------------
 const double EPS = 10e-4;
-
+//-----------------------------------------------------------------------------
+//! @brief Checking for the tendency of the difference of numbers to zero
+//!
+//! @param [in] a, b - Two numbers whose difference is compared with EPS
+//!
+//! @return 0
+//-----------------------------------------------------------------------------
 int comp_eps (const double a, const double b);
 
 #endif
