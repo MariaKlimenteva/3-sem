98af5c68293f7de5d11312d3d60857c4e64a6796
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-09-05 22:06:03 +0300

    Исправила комментарии по оформлению документации

--- Equation/include/Unit_Tests.h
+++ Equation/include/Unit_Tests.h
@@ -2,30 +2,41 @@
 #ifndef UNIT_TESTS_H
 #define UNIT_TESTS_H
 
-// ===============================> READ IT AND APPLY! <===============================
-// TODO: SEE SquareEquation.h FOR COMMON DOCUMENTATION CRITIQUE THAT APPLIES HERE TOO!
-
 //-----------------------------------------------------------------------------
-//! @brief The function outputs that the test has passed, if the test values (set) match
-//! those received by the solve_quadratic_equation function, otherwise it outputs an error message
+//! @brief Compare expected_roots and roots from solve_quadratic_equation. Output a message about passing the test or an error
 //!
-//! @param [in] a, b, c - coefficients defining the quadratic equation ax^2 + bx + c = 0
-//!             test_roots - the calculated number of roots, which we then compare with what the function will return solve_quadratic_equation
-//!             test_x1, test_x2 - calculated the correct values of the roots of the equation that the function solve_quadratic_equation should get
+//! @param [in] a - the coefficient at x^2 in the quadratic equation
 //!
-//! @return returns nothing
-//-----------------------------------------------------------------------------
-void unit_test (double a, double b, double c, int test_roots, double test_x1, double test_x2);
-//-----------------------------------------------------------------------------
-//! @brief Reading text values from a file and using the unit_test function
+//! @param [in] b - the coefficient at x in the quadratic equation
 //!
-//! @param [in] a, b, c - coefficients defining the quadratic equation ax^2 + bx + c = 0
-//!             test_roots - the calculated number of roots, which we then compare with what the function will return solve_quadratic_equation
-//!             test_x1, test_x2 - calculated the correct values of the roots of the equation that the function solve_quadratic_equation should get
+//! @param [in] c - free term in the quadratic equation
+//!
+//! @param [in] expected_roots - the calculated number of roots,  compare them with what solve_quadratic_equation will return
+//!
+//! @param [in] expected_x1 - calculate the correct value of the first root
+//!
+//! @param [in] expected_x2 - calculate the correct value of the second root
 //!
 //! @return returns nothing
 //-----------------------------------------------------------------------------
-void unit_Test_fromFile (double a, double b, double c, int test_roots, double test_x1, double test_x2);
-// TODO: ^~   ^~  ^~ TODO: What is this naming, where have you seen such? Use consistent style, please!
+void unit_test (double a, double b, double c, int expected_roots, double expected_x1, double expected_x2);
+//-----------------------------------------------------------------------------
+//! @brief Read expected values from a file and use the unit_test function
+//!
+//! @param [in] a - the coefficient at x^2 in the quadratic equation
+//!
+//! @param [in] b - the coefficient at x in the quadratic equation
+//!
+//! @param [in] c - free term in the quadratic equation
+//!
+//! @param [in] expected_roots - the calculated number of roots,  compare them with what solve_quadratic_equation will return
+//!
+//! @param [in] expected_x1 - calculate the correct value of the first root
+//!
+//! @param [in] expected_x2 - calculate the correct value of the second root
+//!
+//! @return returns nothing
+//-----------------------------------------------------------------------------
+void unit_test_from_file (double a, double b, double c, int expected_roots, double expected_x1, double expected_x2);
 
 #endif // UNIT_TESTS_H
